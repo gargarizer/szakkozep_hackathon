bfde221e4a8280a247717e6b1df924553d2b839c
gargarizer <zsarnaizsombi@gmail.com>
2024-05-26 16:00:08 +0200

    Update main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,32 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+f=open("1_palindrom_input.txt", "r")
+text=(f.read()).strip().replace(" ", "").lower().split("\n")
+words=[]
+alfanumeric="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".lower()
+characters=[]
+alfanumeric_count=[]
+for i in text:
+    words.append(i)
 
-print(input)
+#palindrome check
+same=0
+palindromes=[]
+for word in words:
+    for i in range(len(word)):
+        if word[i]==word[-i-1]:
+            same+=1
+    if same==len(word):
+        palindromes.append(word)
+    same=0
+#nem szed ki speciális karaktert
+#alfanumerics count
+for i in words:
+    for x in range(len(i)):
+        if i[x] in alfanumeric and i[x] not in characters:
+            characters.append(i[x])
+    alfanumeric_count.append(len(characters))
+    characters=[]
+for i in range(len(words)-1):
+    if words[i] in palindromes:
+        print(f"YES {alfanumeric_count[i]}")
+    else:
+        print(f"NO {alfanumeric_count[i]}")
